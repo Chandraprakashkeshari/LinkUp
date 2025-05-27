308082a601d50365bed84323917ab305baa51b2b
Chandraprakashkeshari <cpkeshari7394@gmail.com>
2025-05-27 12:15:31 +0530

    Add JWT and password validation methods to User model

--- src/app.js
+++ src/app.js
@@ -84,20 +84,20 @@ app.post("/login",async(req,res)=>{
     if(!user){
         throw new Error("Invalid credentials");
     }
-    const isPasswordValid = await bcrypt.compare(password,user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if(isPasswordValid){
 
     //created a jwt token
-    const token = await jwt.sign({ _id: user._id},"LinkUp@VM",{expiresIn:"1d"});
-    console.log(token);
+    const token = await user.getJWT();
+    
 
     //add the token to cookie and the response back to the user
     res.cookie("token", token, 
     {expires: new Date(Date.now()+ 8*360000),
     });
 
-        res.send("login successfully !!")
+    res.send("login successfully !!")
     }else{
         throw new Error ("Invalid credentials")
     }
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt=require("bcrypt");
+const jwt= require("jsonwebtoken");
 
 const UserSchema= new mongoose.Schema({
     firstName:{
@@ -70,6 +72,25 @@ const UserSchema= new mongoose.Schema({
 {
   timestamps:true, // this refers to the this account update itself
 });
+
+UserSchema.methods.getJWT = async function(){
+    const user = this;
+    const token = await jwt.sign({ _id: user._id},"LinkUp@VM",{
+    expiresIn:"7d",
+    });
+    return token;
+}; 
+
+UserSchema.methods.validatePassword = async function (passwordInputByUser){
+    const user =this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(
+        passwordInputByUser,
+        passwordHash
+    );
+    return isPasswordValid;
+};
 
 
 
