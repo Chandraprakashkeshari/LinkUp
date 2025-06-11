dbfb04d7c8b3607247a85d5a1528cdf28525df45
Chandraprakashkeshari <cpkeshari7394@gmail.com>
2025-06-11 21:12:46 +0530

    created user's all connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,8 +5,10 @@ const userRouter = express.Router();
 const {UserAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = " firstName lastName photoURL age gender";
 
-//get all  pending connection  request for the loggedin user
+
+//get all  pending connection  request for the loggedin user;
 userRouter.get("/user/request/received", UserAuth, async (req,res)=>{
     try{
         const loggedinUser = req.user;
@@ -20,6 +22,27 @@ userRouter.get("/user/request/received", UserAuth, async (req,res)=>{
 
     }catch(err){
         req.statusCode  (404).send("ERROR : " + err.message);
+    }
+})
+
+// get all connetion to loggedIn User;
+userRouter.get("/user/connections", UserAuth, async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const connectionReqs = await ConnectionRequest.find({
+            $or: [
+                { toUserId : loggedInUser._id , status : "accepted", },
+                { fromUserId : loggedInUser._id , status : "accepted",},
+            ],  
+        }).populate("fromUserId", USER_SAFE_DATA);
+       
+        const data = connectionReqs.map((row) => row.fromUserId);
+
+        res.json({data: connectionReqs});
+
+    }catch(err){
+        res.status(404).send("ERROR : " + err.message);
     }
 })
 module.exports= userRouter;
